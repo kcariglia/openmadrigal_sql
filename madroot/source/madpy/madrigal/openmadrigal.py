380c8a31191d1b23d5e6d73d2a356740cd5e0a0a
cariglia <cariglia@mit.edu>
2025-02-10 09:50:37 -0500

    beijing site also still uses http

--- madroot/source/madpy/madrigal/openmadrigal.py
+++ madroot/source/madpy/madrigal/openmadrigal.py
@@ -117,10 +117,10 @@ class OpenMadrigal:
             urlStr = serverName +  \
                      '/getMetadata?fileType=' + str(metadataType)
         if urlStr.find('http') == -1:
-            if siteId not in (1, 22):
+            if siteId not in (1, 8, 22):
                 urlStr = self._webProtocol + '://' + urlStr
             else:
-                # Millstone and Illinois sites still uses http
+                # Millstone, Beijing, and Illinois sites still uses http
                 urlStr = "http" + '://' + urlStr
         
         
